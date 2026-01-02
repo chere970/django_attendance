26670b0fc7e7a13ecf5cb77bbf68d566ff77ecb3
cherinet <cherinetkebede055@gmail.com>
2026-01-02 23:33:39 +0300

    new  endpoint arcreated

--- django/reports/urls.py
+++ django/reports/urls.py
@@ -1,7 +1,9 @@
 from django.urls import path
 from .views import ReportView
 from .views import LeaveRequestView
+from .views import SendLeaveRequest
 urlpatterns = [
     path('reports/', ReportView, name='reports'),
     path('leave-requests/', LeaveRequestView, name='leave-requests'),
+    path('leave-request/add/', SendLeaveRequest, name='send-leave-request'),
 ]
