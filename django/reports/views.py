080798e88643235d19ec4120d7dae97e7df69ee3
cherinet <cherinetkebede055@gmail.com>
2026-01-02 23:24:30 +0300

    Add SendLeaveRequest view to handle leave request submissions

--- django/reports/views.py
+++ django/reports/views.py
@@ -25,3 +25,12 @@ def LeaveRequestView(request):
 
     leave_requests = serializer.data
     return Response(leave_requests)
+
+
+@api_view(['POST'])
+def SendLeaveRequest(request):
+    serializer = LeaveRequestSerializer(data=request.data)
+    if serializer.is_valid():
+        serializer.save()
+        return Response(serializer.data, status=201)
+    return Response(serializer.errors, status=400)
